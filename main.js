50d241d303243d161ac0bf127e5f30548c16a9b5
Matthew <matt@sigouin.codes>
2024-02-19 07:13:02 -0500

    crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const { crawlPage } = require("./crawl");
+
+function main() {
+  if (process.argv.length < 3) {
+    console.error("Less than one");
+    process.exit(1);
+  } else if (process.argv.length > 3) {
+    console.error("More than one");
+    process.exit(1);
+  } else {
+    const baseURL = process.argv[2];
+    console.log(`Crawler is starting at ${baseURL}`);
+    crawlPage(baseURL);
+  }
+}
+
+main();
